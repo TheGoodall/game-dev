2ec8805db6449d080c3a9715d0d9b5280711af84
James Goodall <jatgoodall@protonmail.com>
2020-12-27 05:34:12 +0000

    Started adding game state and difficulty

--- DontWaitVaccinate/game.py
+++ DontWaitVaccinate/game.py
@@ -1,3 +1,9 @@
 class game():
     def __init__(self):
         self.running = True
+        self.game_state = False
+
+class game_state():
+    difficulty_options = {"easy":{}, "medium":{}, "hard":{}}
+    def __init__(self, difficulty):
+        pass
