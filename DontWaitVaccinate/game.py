6414dedd8e745b0567f52eace51383797ec48cc3
James Goodall <jatgoodall@protonmail.com>
2020-12-27 06:02:23 +0000

    implemented difficulty options as an enum

--- DontWaitVaccinate/game.py
+++ DontWaitVaccinate/game.py
@@ -1,12 +1,25 @@
+from enum import Enum
+
+
 class game():
+    class difficulty_options(Enum):
+        easy = {},
+        medium = {},
+        hard = {}
+
     def __init__(self):
         self.running = True
         self.game_state = False
+        self.paused = False
+
+    def start(self, difficulty):
+        self.game_state = game_state(difficulty)
+
 
 class game_state():
-    difficulty_options = {"easy":{}, "medium":{}, "hard":{}}
     def __init__(self, difficulty):
-        self.player = Player(self.difficulty_options[difficulty]["player_health"])
+        self.player = Player(difficulty["player_health"])
+
 
 class Player():
     def __init__(self, health):
