d08344e5b1d43a6b38a8441694b7cdccfeb9eb32
James Goodall <jatgoodall@protonmail.com>
2020-12-28 01:33:29 +0000

    Added menu class and moved relevent code

--- DontWaitVaccinate/menu.py
+++ DontWaitVaccinate/menu.py
@@ -1,11 +1,25 @@
-class home_screen():
+class menu():
     """ Contains menu implementation and state """
 
     def __init__(self, game_instance):
         self.game_instance = game_instance
+        self.buttons = []
 
     def render(self, surface):
-        pass
+        for button in self.buttons:
+            button.render(surface)
 
     def process_event(self, event):
         pass
+
+
+class home_screen(menu):
+    """ Contains the implementation of menu specific to the main menu """
+
+    def __init__(self, game_instance):
+        super().__init__(game_instance)
+        self.buttons = []
+
+
+class button():
+    """ contains implementation of buttons """
