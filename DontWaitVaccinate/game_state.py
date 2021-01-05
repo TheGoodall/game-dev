11f5c1c82b4e8b2d45e77c6355f15ef3c2da6ee3
James Goodall <jatgoodall@protonmail.com>
2021-01-05 07:24:15 +0000

    added rendering functions

--- DontWaitVaccinate/game_state.py
+++ DontWaitVaccinate/game_state.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class game_state():
     """ Contains the current game state, including the player, the world, and any NPCs """
     # Initialise with difficulty options (d)
@@ -10,6 +13,10 @@ class game_state():
         # Initialise World
         self.world = World(d['size'], d['density'])
 
+    def render(self, surface, font):
+        self.world.render(surface, font)
+        self.player.render(surface, font)
+
 
 class Player():
     """ Contains the current state of the player """
@@ -17,9 +24,15 @@ class Player():
     def __init__(self, health) -> None:
         self.health = health
 
+    def render(self, surface, font):
+        pass
+
 
 class World():
     """ Contains the current state of the world, e.g. buildings """
 
     def __init__(self, size, density) -> None:
+        pass
+
+    def render(self, surface, font):
         pass
