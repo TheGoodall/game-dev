aab815f31d0cb2c1d5ea734659cf8a5dc2921ce5
James Goodall <jatgoodall@protonmail.com>
2021-01-18 15:44:49 +0000

    attempt to add sprites

--- DontWaitVaccinate/game_state.py
+++ DontWaitVaccinate/game_state.py
@@ -1,5 +1,6 @@
 import pygame
 
+from . import spritesheet
 
 class game_state():
     """ Contains the current game state, including the player, the world, and any NPCs """
@@ -9,15 +10,20 @@ class game_state():
 
         self.cam_pos = (0, 0)
 
+
+        spritesheet1 = spritesheet.spritesheet("DontWaitVaccinate/images/spritesheet.png")
+        spritesheet2 = spritesheet.spritesheet("DontWaitVaccinate/images/spritesheet 2.png")
+        
+
         # Initialise Player
-        self.player = Player()
+        self.player = Player(spritesheet1.get_images(0,0))
 
         # Initialise World
         self.world = World(d['size'], d['density'])
 
     def render(self, surface, font):
         """ Render every subobject of game state """
-        self.world.render(surface, font, self.cam_pos)
+        self.world.render_ground(surface, font, self.cam_pos)
         self.player.render(surface, font, self.cam_pos)
 
     def process_event(self, event) -> None:
@@ -41,14 +47,28 @@ class game_state():
 class PhysicalObject():
     def __init__(self, pos):
         self.pos = pos
-    def render(self, surface, font, cam_pos):
+    def render(self, surface, font, cam_pos, sprite):
         """ Render Entity """
-        pass
-
+        pos = (self.pos[0]-cam_pos[0], self.pos[1]-cam_pos[1])
+        print(sprite)
+        pygame.Surface.blit(sprite, surface, pos)
 
 class Entity(PhysicalObject):
-    def __init__(self, pos):
+    def __init__(self, pos, sprites):
+        self.sprites = sprites
+        self.sprite_state = 0
+        self.sprite_dir = 1
         super().__init__(pos)
+
+    def render(self, surface, font, cam_pos):
+        sprites = self.sprites[self.sprite_dir]
+        if self.sprite_state == 3:
+            sprite = sprites[1]
+        else:
+            sprite = sprites[self.sprite_state]
+
+        super().render(surface, font, cam_pos, sprite)
+
 
 
 class NPC(Entity):
@@ -58,8 +78,8 @@ class NPC(Entity):
 class Player(Entity):
     """ Contains the current state of the player """
 
-    def __init__(self) -> None:
-        super().__init__((0,0))
+    def __init__(self, sprites) -> None:
+        super().__init__((0,0), sprites)
 
 
 class World():
@@ -71,10 +91,6 @@ class World():
         self.ground_texture = pygame.transform.scale(
             self.ground_texture, (500, 500))
 
-    def render(self, surface, font, cam_pos):
-        """ Render world """
-        self.render_ground(surface, font, cam_pos)
-
     def render_ground(self, surface, font, cam_pos):
         screen_size = surface.get_size()
         texture_size = self.ground_texture.get_size()
@@ -83,11 +99,8 @@ class World():
         render_offset = -(cam_pos[0] % texture_size[0]
                           ), -(cam_pos[1] % texture_size[1])
 
-        print(screen_size, texture_size)
-
         count = round(screen_size[0] / texture_size[0] +
                       1), round(screen_size[0] / texture_size[0] + 1)
-        print(count)
 
         render = []
 
---
+++ DontWaitVaccinate/spritesheet.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+class spritesheet():
+    def __init__(self, image_path: str) -> None:
+        self.sheet = pygame.image.load(image_path)
+    
+    def get_image(self, x, y, x_size, y_size):
+        image = pygame.Surface((x_size, y_size))
+        image.blit(self.sheet, (0,0), pygame.Rect(x, y, x_size, y_size))
+        return image
+
+    def get_images(self, x, y):
+        y = 4 * y
+        down =     [self.get_image((x+i)*48, y*48, 48, 48) for i in range(3)]
+        left =     [self.get_image((x+i)*48, (y+1)*48, 48, 48) for i in range(3)]
+        right =    [self.get_image((x+i)*48, (y+2)*48, 48, 48) for i in range(3)]
+        up =       [self.get_image((x+i)*48, (y+3)*48, 48, 48) for i in range(3)]
+        return [up, right, down, left]
+
